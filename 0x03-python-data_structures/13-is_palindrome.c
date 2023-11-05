dd5f849ffcb3b939ef912de250b83567be51c48f
Eunice-Serani <eaamable91@gmail.com>
2023-11-05 08:09:05 -0800

    My palindrome file

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,53 +1,36 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
-*add_nodeint - Adds a new node at the beginning of a listint_t list.
-*@head: head of listint_t.
-*@n: int to add in listint_t list.
-*Return: Address of the new element, or NULL if it failed.
-*/
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	return (new);
-}
-/**
-*is_palindrome - Identifies if a syngle linked list is palindrome.
-*@head: head of listint_t.
-*Return: 1 if it is palindrome else 0.
-*/
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: Points to the pointer to the head of the list.
+ *
+ * Return: 1 if list is palindrome, else 0.
+ */
 int is_palindrome(listint_t **head)
 {
-	listint_t *head2 = *head;
-	listint_t *aux = NULL, *aux2 = NULL;
+	int k, count = 0;
+	int list[1024];
+	listint_t *current = *head;
 
-	if (*head == NULL || head2->next == NULL)
+	if (*head == NULL || (*head)->next == NULL)
+	{
 		return (1);
-	while (head2 != NULL)
-	{
-		add_nodeint(&aux, head2->n);
-		head2 = head2->next;
 	}
-	aux2 = aux;
-	while (*head != NULL)
+
+	while (current)
 	{
-		if ((*head)->n != aux2->n)
+		list[count] = current->n;
+		current = current->next;
+		count += 1;
+	}
+
+	for (k = 0; k < count / 2; k++)
+	{
+		if (list[k] != list[count - 1 - k])
 		{
-			free_listint(aux);
 			return (0);
 		}
-		*head = (*head)->next;
-		aux2 = aux2->next;
 	}
-	free_listint(aux);
+
 	return (1);
 }
