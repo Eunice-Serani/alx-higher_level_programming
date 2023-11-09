4fb838dd4c893ffab827b5c098af4f16a21d103a
Eunice-Serani <eaamable91@gmail.com>
2023-11-09 04:31:17 +0000

    Python roman integer converter

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,13 +2,10 @@
 def roman_to_int(roman_string):
 	if not roman_string or type(roman_string) != str:
 		return 0
-	roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-	roman_n = 0
-	for k in range(len(roman_string)):
-		if k > 0 and roman_d[roman_string[k]] > roman_d[roman_string[k - 1]]:
-			roman_n += roman_d[roman_string[k]] - 2 * \
-				roman_d[roman_string[k - 1]]
-		else:
-			roman_n += roman_d[roman_string[k]]
-	return roman_n
-
+	total = 0
+	package = 0
+	roman_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+	for i in reversed(roman_string):
+		package = roman_num[i]
+		total += package if total < package * 5 else -package
+	return total
