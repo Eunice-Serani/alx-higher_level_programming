5e7f42e42e4fba0e9a15f55ac8ffaca3e0c85f34
Eunice-Serani <eaamable91@gmail.com>
2023-11-29 05:24:56 +0000

    Python test driven

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""
+0-add_integer.py
+
+Defines integer addition function
+Usage:
+add_integer(3, 7)
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds two integers and returns the sum.
+
+    Parameters:
+    a (int or float): The first integer or float.
+    b (int or float, optional): The second integer or float. Defaults to 98.
+
+    Returns:
+    int: The sum of a and b, casted to an integer.
+
+    Raises:
+    TypeError: If a or b is not an integer or float.
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    a = int(a)
+    b = int(b)
+
+    return int(a) + int(b)
