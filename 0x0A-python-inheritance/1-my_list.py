875aca2ff120dce06b4acb45ad3c000cfac233ad
Eunice-Serani <eaamable91@gmail.com>
2023-12-06 13:45:15 +0000

    Python inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-"""Defines an inherited list class MyList."""
+# my_list
+"""
+Implements a class that inherits from list
+"""
 
 
 class MyList(list):
-    """Implements sorted printing for the built-in list class."""
+    """
+    MyList class
+
+    Inherits from list
+    """
 
     def print_sorted(self):
-        """Print a list in sorted ascending order."""
-        print(sorted(self))
+        """
+        Prints the list but sorted in ascending order
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
-"""
-This contains BaseGeometry
-with public instance method area and integer_validation
-"""
+# 7-base_geometry
+"""Implements a BaseGeometry class"""
 
 
 class BaseGeometry:
-    """
-    Methods:
-        area(self)
-        integer_validator(self, name, value)
-    """
+    """A class with a public instance methods"""
+
     def area(self):
-        """not implemented"""
+        """
+        Raises an exception with a message
+
+        Raises:
+        Exception: with a message "area() is not implemented"
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """validates input
-        Args:
-            name (str): assumed always a string
-            value (int): greater than 0
+        """
+        Validates value
+
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is less or equal to 0
         """
         if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+            raise ValueError(f"{name} must be greater than 0")
