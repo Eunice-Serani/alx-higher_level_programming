d126bf5c60670cf2d653fade3045d07115eb60ff
Eunice-Serani <eaamable91@gmail.com>
2024-02-29 05:51:25 -0800

    Finding peak integers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,6 +2,7 @@
 """ This finds a peak in a list of unsorted integers
 """
 
+
 def find_peak(list_of_integers):
     """
     Args:
@@ -21,14 +22,14 @@ def find_peak(list_of_integers):
     while True:
         mid_range = mid_range // 2
 
-    if (mid < size - 1 and
-            list_of_integers[mid] < list_of_integers[mid + 1]):
-        if mid_range // 2 == 0:
-            mid_range = 2
-                mid = mid + mid_range // 2
-            elif mid_range > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        if (mid < size - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
             if mid_range // 2 == 0:
                 mid_range = 2
-                    mid = mid - mid_range // 2
-                else:
-                    return list_of_integers[mid]
+            mid = mid + mid_range // 2
+        elif mid_range > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            if mid_range // 2 == 0:
+                mid_range = 2
+            mid = mid - mid_range // 2
+        else:
+            return list_of_integers[mid]
