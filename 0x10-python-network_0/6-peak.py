33566f52c65ecce0c57d03110de53f1e9766893b
Eunice-Serani <eaamable91@gmail.com>
2024-02-29 05:17:25 -0800

    Network scripting

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,26 +6,24 @@
 def find_peak(list_of_integers):
     """
     Args:
-        list_of_integers(int): list of integers to find peak of
-    Returns: peak of list_of_integers or None
+        list_of_integers (list of int): List of integers to find the peak of
+    Returns:
+        int or None: Peak of list_of_integers or None if list is empty
     """
     size = len(list_of_integers)
-    mid_e = size
-    mid = size // 2
 
     if size == 0:
         return None
 
-    while True:
-        mid_e = mid_e // 2
-        if (mid < size - 1 and
-                list_of_integers[mid] < list_of_integers[mid + 1]):
-            if mid_e // 2 == 0:
-                mid_e = 2
-            mid = mid + mid_e // 2
-        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-            if mid_e // 2 == 0:
-                mid_e = 2
-            mid = mid - mid_e // 2
+    low, high = 0, size - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            low = mid + 1
         else:
-          return list_of_integers[mid]
+            high = mid
+
+    return list_of_integers[low]
